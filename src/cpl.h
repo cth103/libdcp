b6488655b833ecffa6b7934cd83cced49571868c
Carl Hetherington <cth@carlh.net>
2012-08-17 23:47:30 +0100

    GPL boilerplate and a few comments.

--- src/cpl.h
+++ src/cpl.h
@@ -1,9 +1,33 @@
+/*
+    Copyright (C) 2012 Carl Hetherington <cth@carlh.net>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+*/
+
+/** @file  src/cpl.h
+ *  @brief Classes used to parse a CPL.
+ */
+
 #include <stdint.h>
 #include <boost/shared_ptr.hpp>
 #include "xml.h"
 
 namespace libdcp {
 
+/** CPL MainPicture node */	
 class MainPicture : public XMLNode
 {
 public:
@@ -20,6 +44,7 @@ public:
 	Fraction screen_aspect_ratio;
 };
 
+/** CPL MainSound node */	
 class MainSound : public XMLNode
 {
 public:
@@ -34,6 +59,7 @@ public:
 	int64_t duration;
 };
 
+/** CPL MainSubtitle node */	
 class MainSubtitle : public XMLNode
 {
 public:
@@ -48,6 +74,7 @@ public:
 	int64_t duration;
 };
 
+/** CPL AssetList node */	
 class CPLAssetList : public XMLNode
 {
 public:
@@ -59,6 +86,7 @@ public:
 	boost::shared_ptr<MainSubtitle> main_subtitle;
 };
 
+/** CPL Reel node */	
 class Reel : public XMLNode
 {
 public:
@@ -69,6 +97,7 @@ public:
 	boost::shared_ptr<CPLAssetList> asset_list;
 };
 
+/** CPL ContentVersion node */	
 class ContentVersion : public XMLNode
 {
 public:
@@ -79,9 +108,11 @@ public:
 	std::string label_text;
 };
 
+/** Class to parse a CPL */
 class CPL : public XMLFile
 {
 public:
+	/** Parse a CPL XML file into our member variables */
 	CPL (std::string file);
 
 	std::string id;
