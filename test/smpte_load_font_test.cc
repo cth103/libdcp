f8b49d20c690d19e9cd5dd9cddc3a834e526ba87
Carl Hetherington <cth@carlh.net>
2014-12-28 02:50:49 +0000

    Another test.

---
+++ test/smpte_load_font_test.cc
@@ -0,0 +1,36 @@
+/*
+    Copyright (C) 2014 Carl Hetherington <cth@carlh.net>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+*/
+
+#include "smpte_load_font.h"
+#include <libcxml/cxml.h>
+#include <libxml++/libxml++.h>
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE (smpte_load_font_test1)
+{
+	xmlpp::Document doc;
+	xmlpp::Element* text = doc.create_root_node("Font");
+
+	text->set_attribute("ID", "my-great-id");
+	text->add_child_text("urn:uuid:my-great-urn");
+	dcp::SMPTELoadFont lf (cxml::ConstNodePtr (new cxml::Node (text)));
+
+	BOOST_CHECK_EQUAL (lf.id, "my-great-id");
+	BOOST_CHECK_EQUAL (lf.urn, "my-great-urn");
+}
