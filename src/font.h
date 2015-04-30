ac345628f3e96ff9872919826795dbee34135aa5
Carl Hetherington <cth@carlh.net>
2015-04-30 21:22:06 +0100

    Add basic Font asset class.

---
+++ src/font.h
@@ -0,0 +1,37 @@
+/*
+    Copyright (C) 2015 Carl Hetherington <cth@carlh.net>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+*/
+
+/** @file  src/font.h
+ *  @brief A (truetype) font asset for subtitles in a DCP.
+ */
+
+#include "asset.h"
+
+namespace dcp {
+
+class Font : public Asset
+{
+public:
+	Font (boost::filesystem::path file);
+	
+private:
+	std::string pkl_type (Standard standard) const;
+};
+
+}
