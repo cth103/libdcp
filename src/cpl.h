8f7a014e6d11c107b520ac3c869b1cc7bef1bbd0
Carl Hetherington <cth@carlh.net>
2016-02-29 00:36:51 +0000

    Fix erroneous extra character.

--- src/cpl.h
+++ src/cpl.h
@@ -1,7 +1,7 @@
 /*
     Copyright (C) 2014 Carl Hetherington <cth@carlh.net>
 
-    This program is free software; you -can redistribute it and/or modify
+    This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation; either version 2 of the License, or
     (at your option) any later version.
