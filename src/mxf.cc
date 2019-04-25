23b87f5e8a0fd0f351e4ae34263beb6c5fd1981a
Carl Hetherington <cth@carlh.net>
2019-04-25 15:46:14 +0000

    Throw a different exception when an unrecognised LabelSetType is found.

--- src/mxf.cc
+++ src/mxf.cc
@@ -135,7 +135,7 @@ MXF::read_writer_info (ASDCP::WriterInfo const & info)
 		_standard = SMPTE;
 		break;
 	default:
-		DCP_ASSERT (false);
+		throw DCPReadError ("Unrecognised label set type in MXF");
 	}
 
 	_metadata.read (info);
