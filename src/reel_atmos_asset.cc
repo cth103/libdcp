56286bed00c2a6ad372b6c9d4902c74fd086f384
Carl Hetherington <cth@carlh.net>
2020-06-19 13:15:13 +0200

    Fix missing Key ID when writing details of encrypted Atmos assets to CPL.

--- src/reel_atmos_asset.cc
+++ src/reel_atmos_asset.cc
@@ -83,6 +83,7 @@ xmlpp::Node *
 ReelAtmosAsset::write_to_cpl (xmlpp::Node* node, Standard standard) const
 {
 	xmlpp::Node* asset = write_to_cpl_asset (node, standard, hash());
+	write_to_cpl_mxf (asset);
 	asset->add_child("axd:DataType")->add_child_text("urn:smpte:ul:060e2b34.04010105.0e090604.00000000");
 	return asset;
 }
