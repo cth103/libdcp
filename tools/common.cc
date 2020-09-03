fc205951d1fec50d59bfbed9cb03c07cc95205d9
Carl Hetherington <cth@carlh.net>
2020-09-03 12:09:53 +0200

    Missing note type when filtering out missing asset errors.

--- tools/common.cc
+++ tools/common.cc
@@ -46,7 +46,7 @@ dcp::filter_notes (list<dcp::VerificationNote>& notes, bool ignore_missing_asset
 		list<dcp::VerificationNote>::iterator tmp = i;
 		++tmp;
 
-		if (ignore_missing_assets && i->code() == dcp::VerificationNote::MISSING_ASSET) {
+		if (ignore_missing_assets && (i->code() == dcp::VerificationNote::MISSING_ASSET || i->code() == dcp::VerificationNote::EXTERNAL_ASSET)) {
 			notes.erase (i);
 		}
 
