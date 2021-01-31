cth103/libdcp

commit 6c37cc1979b2a01205a888c4c98f3334685ee8dd

Tidying.

src/certificate_chain.cc
@@ -1,5 +1,5 @@
 /*
-    Copyright (C) 2013-2016 Carl Hetherington <cth@carlh.net>
+    Copyright (C) 2013-2021 Carl Hetherington <cth@carlh.net>
 
     This file is part of libdcp.
 
@@ -31,9 +31,11 @@
     files in the program, then also delete it here.
 */
 
-/** @file  src/signer_chain.cc
- *  @brief Functions to make signer chains.
+
+/** @file  src/certificate_chain.cc
+ *  @brief CertificateChain class
  */
+
 
 #include "certificate_chain.h"
 #include "exceptions.h"
@@ -57,11 +59,13 @@
 #include <fstream>
 #include <iostream>
 
+
 using std::string;
 using std::ofstream;
 using std::ifstream;
 using std::runtime_error;
 using namespace dcp;
+
 
 /** Run a shell command.
  *  @param cmd Command to run (UTF8-encoded).
@@ -74,7 +78,7 @@ command (string cmd)
 	   is handled correctly.
 	*/
 	int const wn = MultiByteToWideChar (CP_UTF8, 0, cmd.c_str(), -1, 0, 0);
-	wchar_t* buffer = new wchar_t[wn];
+	char buffer = new wchar_t[wn];
 	if (MultiByteToWideChar (CP_UTF8, 0, cmd.c_str(), -1, buffer, wn) == 0) {
 		delete[] buffer;
 		return;
@@ -111,6 +115,7 @@ command (string cmd)
 	}
 }
 
+
 /** Extract a public key from a private key and create a SHA1 digest of it.
  *  @param private_key Private key
  *  @param openssl openssl binary name (or full path if openssl is not on the system path).
@@ -122,12 +127,12 @@ public_key_digest (boost::filesystem::path private_key, boost::filesystem::path 
 	boost::filesystem::path public_name = private_key.string() + ".public";
 
 	/* Create the public key from the private key */
-	command (String::compose("\"%1\" rsa -outform PEM -pubout -in %2 -out %3", openssl.string(), private_key.string(), public_name.string ()));
+	command (String::compose("\"%1\" rsa -outform PEM -pubout -in %2 -out %3", openssl.string(), private_key.string(), public_name.string()));
 
 	/* Read in the public key from the file */
 
 	string pub;
-	ifstream f (public_name.string().c_str ());
+	ifstream f (public_name.string().c_str());
 	if (!f.good ()) {
 		throw dcp::MiscError ("public key not found");
 	}
@@ -176,6 +181,7 @@ public_key_digest (boost::filesystem::path private_key, boost::filesystem::path 
 	return dig;
 }
 
+
 CertificateChain::CertificateChain (
 	boost::filesystem::path openssl,
 	string organisation,
@@ -188,10 +194,10 @@ CertificateChain::CertificateChain (
 	/* Valid for 40 years */
 	int const days = 365 * 40;
 
-	boost::filesystem::path directory = boost::filesystem::temp_directory_path() / boost::filesystem::unique_path ();
+	auto directory = boost::filesystem::temp_directory_path() / boost::filesystem::unique_path ();
 	boost::filesystem::create_directories (directory);
 
-	boost::filesystem::path const cwd = boost::filesystem::current_path ();
+	auto const cwd = boost::filesystem::current_path ();
 	boost::filesystem::current_path (directory);
 
 	string quoted_openssl = "\"" + openssl.string() + "\"";
@@ -314,14 +320,15 @@ CertificateChain::CertificateChain (
 
 	boost::filesystem::current_path (cwd);
 
-	_certificates.push_back (dcp::Certificate (dcp::file_to_string (directory / "ca.self-signed.pem")));
-	_certificates.push_back (dcp::Certificate (dcp::file_to_string (directory / "intermediate.signed.pem")));
-	_certificates.push_back (dcp::Certificate (dcp::file_to_string (directory / "leaf.signed.pem")));
+	_certificates.push_back (dcp::Certificate(dcp::file_to_string(directory / "ca.self-signed.pem")));
+	_certificates.push_back (dcp::Certificate(dcp::file_to_string(directory / "intermediate.signed.pem")));
+	_certificates.push_back (dcp::Certificate(dcp::file_to_string(directory / "leaf.signed.pem")));
 
 	_key = dcp::file_to_string (directory / "leaf.key");
 
 	boost::filesystem::remove_all (directory);
 }
+
 
 CertificateChain::CertificateChain (string s)
 {
@@ -340,30 +347,31 @@ CertificateChain::CertificateChain (string s)
 	leaf_to_root ();
 }
 
-/** @return Root certificate */
+
 Certificate
 CertificateChain::root () const
 {
 	DCP_ASSERT (!_certificates.empty());
-	return root_to_leaf().front ();
+	return root_to_leaf().front();
 }
 
-/** @return Leaf certificate */
+
 Certificate
 CertificateChain::leaf () const
 {
 	DCP_ASSERT (!_certificates.empty());
-	return root_to_leaf().back ();
+	return root_to_leaf().back();
 }
 
-/** @return Certificates in order from leaf to root */
+
 CertificateChain::List
 CertificateChain::leaf_to_root () const
 {
-	List l = root_to_leaf ();
+	auto l = root_to_leaf ();
 	std::reverse (l.begin(), l.end());
 	return l;
 }
+
 
 CertificateChain::List
 CertificateChain::unordered () const
@@ -371,18 +379,14 @@ CertificateChain::unordered () const
 	return _certificates;
 }
 
-/** Add a certificate to the chain.
- *  @param c Certificate to add.
- */
+
 void
 CertificateChain::add (Certificate c)
 {
 	_certificates.push_back (c);
 }
 
-/** Remove a certificate from the chain.
- *  @param c Certificate to remove.
- */
+
 void
 CertificateChain::remove (Certificate c)
 {
@@ -392,13 +396,11 @@ CertificateChain::remove (Certificate c)
 	}
 }
 
-/** Remove the i'th certificate in the list, as listed
- *  from root to leaf.
- */
+
 void
 CertificateChain::remove (int i)
 {
-	List::iterator j = _certificates.begin ();
+	auto j = _certificates.begin ();
         while (j != _certificates.end () && i > 0) {
 		--i;
 		++j;
@@ -409,17 +411,14 @@ CertificateChain::remove (int i)
 	}
 }
 
+
 bool
 CertificateChain::chain_valid () const
 {
 	return chain_valid (_certificates);
 }
 
-/** Check to see if a chain is valid (i.e. root signs the intermediate, intermediate
- *  signs the leaf and so on) and that the private key (if there is one) matches the
- *  leaf certificate.
- *  @return true if it's ok, false if not.
- */
+
 bool
 CertificateChain::chain_valid (List const & chain) const
 {
@@ -430,29 +429,29 @@ CertificateChain::chain_valid (List const & chain) const
 	   any time soon.
 	*/
 
-	X509_STORE* store = X509_STORE_new ();
+	auto store = X509_STORE_new ();
 	if (!store) {
 		throw MiscError ("could not create X509 store");
 	}
 
 	/* Put all the certificates into the store */
-	for (List::const_iterator i = chain.begin(); i != chain.end(); ++i) {
-		if (!X509_STORE_add_cert (store, i->x509 ())) {
-			X509_STORE_free (store);
+	for (auto const& i: chain) {
+		if (!X509_STORE_add_cert(store, i.x509())) {
+			X509_STORE_free(store);
 			return false;
 		}
 	}
 
 	/* Verify each one */
-	for (List::const_iterator i = chain.begin(); i != chain.end(); ++i) {
+	for (auto i = chain.begin(); i != chain.end(); ++i) {
 
-		List::const_iterator j = i;
+		auto j = i;
 		++j;
 		if (j == chain.end ()) {
 			break;
 		}
 
-		X509_STORE_CTX* ctx = X509_STORE_CTX_new ();
+		auto ctx = X509_STORE_CTX_new ();
 		if (!ctx) {
 			X509_STORE_free (store);
 			throw MiscError ("could not create X509 store context");
@@ -489,9 +488,7 @@ CertificateChain::chain_valid (List const & chain) const
 	return true;
 }
 
-/** Check that there is a valid private key for the leaf certificate.
- *  Will return true if there are no certificates.
- */
+
 bool
 CertificateChain::private_key_valid () const
 {
@@ -503,17 +500,17 @@ CertificateChain::private_key_valid () const
 		return false;
 	}
 
-	BIO* bio = BIO_new_mem_buf (const_cast<char *> (_key->c_str ()), -1);
+	auto bio = BIO_new_mem_buf (const_cast<char *> (_key->c_str ()), -1);
 	if (!bio) {
 		throw MiscError ("could not create memory BIO");
 	}
 
-	RSA* private_key = PEM_read_bio_RSAPrivateKey (bio, 0, 0, 0);
+	auto private_key = PEM_read_bio_RSAPrivateKey (bio, 0, 0, 0);
 	if (!private_key) {
 		return false;
 	}
 
-	RSA* public_key = leaf().public_key ();
+	auto public_key = leaf().public_key ();
 
 #if OPENSSL_VERSION_NUMBER > 0x10100000L
 	BIGNUM const * private_key_n;
@@ -531,6 +528,7 @@ CertificateChain::private_key_valid () const
 
 	return valid;
 }
+
 
 bool
 CertificateChain::valid (string* reason) const
@@ -554,11 +552,11 @@ CertificateChain::valid (string* reason) const
 	return true;
 }
 
-/** @return Certificates in order from root to leaf */
+
 CertificateChain::List
 CertificateChain::root_to_leaf () const
 {
-	List rtl = _certificates;
+	auto rtl = _certificates;
 	std::sort (rtl.begin(), rtl.end());
 	do {
 		if (chain_valid (rtl)) {
@@ -569,20 +567,17 @@ CertificateChain::root_to_leaf () const
 	throw CertificateChainError ("certificate chain is not consistent");
 }
 
-/** Add a &lt;Signer&gt; and &lt;ds:Signature&gt; nodes to an XML node.
- *  @param parent XML node to add to.
- *  @param standard INTEROP or SMPTE.
- */
+
 void
 CertificateChain::sign (xmlpp::Element* parent, Standard standard) const
 {
 	/* <Signer> */
 
 	parent->add_child_text("  ");
-	xmlpp::Element* signer = parent->add_child("Signer");
+	auto signer = parent->add_child("Signer");
 	signer->set_namespace_declaration ("http://www.w3.org/2000/09/xmldsig#", "dsig");
-	xmlpp::Element* data = signer->add_child("X509Data", "dsig");
-	xmlpp::Element* serial_element = data->add_child("X509IssuerSerial", "dsig");
+	auto data = signer->add_child("X509Data", "dsig");
+	auto serial_element = data->add_child("X509IssuerSerial", "dsig");
 	serial_element->add_child("X509IssuerName", "dsig")->add_child_text (leaf().issuer());
 	serial_element->add_child("X509SerialNumber", "dsig")->add_child_text (leaf().serial());
 	data->add_child("X509SubjectName", "dsig")->add_child_text (leaf().subject());
@@ -592,12 +587,12 @@ CertificateChain::sign (xmlpp::Element* parent, Standard standard) const
 	/* <Signature> */
 
 	parent->add_child_text("\n  ");
-	xmlpp::Element* signature = parent->add_child("Signature");
+	auto signature = parent->add_child("Signature");
 	signature->set_namespace_declaration ("http://www.w3.org/2000/09/xmldsig#", "dsig");
 	signature->set_namespace ("dsig");
 	parent->add_child_text("\n");
 
-	xmlpp::Element* signed_info = signature->add_child ("SignedInfo", "dsig");
+	auto signed_info = signature->add_child ("SignedInfo", "dsig");
 	signed_info->add_child("CanonicalizationMethod", "dsig")->set_attribute ("Algorithm", "http://www.w3.org/TR/2001/REC-xml-c14n-20010315");
 
 	if (standard == Standard::INTEROP) {
@@ -606,10 +601,10 @@ CertificateChain::sign (xmlpp::Element* parent, Standard standard) const
 		signed_info->add_child("SignatureMethod", "dsig")->set_attribute("Algorithm", "http://www.w3.org/2001/04/xmldsig-more#rsa-sha256");
 	}
 
-	xmlpp::Element* reference = signed_info->add_child("Reference", "dsig");
+	auto reference = signed_info->add_child("Reference", "dsig");
 	reference->set_attribute ("URI", "");
 
-	xmlpp::Element* transforms = reference->add_child("Transforms", "dsig");
+	auto transforms = reference->add_child("Transforms", "dsig");
 	transforms->add_child("Transform", "dsig")->set_attribute (
 		"Algorithm", "http://www.w3.org/2000/09/xmldsig#enveloped-signature"
 		);
@@ -624,11 +619,6 @@ CertificateChain::sign (xmlpp::Element* parent, Standard standard) const
 }
 
 
-/** Sign an XML node.
- *
- *  @param parent Node to sign.
- *  @param ns Namespace to use for the signature XML nodes.
- */
 void
 CertificateChain::add_signature_value (xmlpp::Element* parent, string ns, bool add_indentation) const
 {
@@ -671,6 +661,7 @@ CertificateChain::add_signature_value (xmlpp::Element* parent, string ns, bool a
 
 	xmlSecDSigCtxDestroy (signature_context);
 }
+
 
 string
 CertificateChain::chain () const

src/asset_reader.h
@@ -1,5 +1,5 @@
 /*
-    Copyright (C) 2016 Carl Hetherington <cth@carlh.net>
+    Copyright (C) 2016-2021 Carl Hetherington <cth@carlh.net>
 
     This file is part of libdcp.
 
@@ -31,33 +31,39 @@
     files in the program, then also delete it here.
 */
 
+
 #ifndef LIBDCP_ASSET_READER_H
 #define LIBDCP_ASSET_READER_H
 
-#include "dcp_assert.h"
+
 #include "asset.h"
 #include "crypto_context.h"
+#include "dcp_assert.h"
 #include <asdcp/AS_DCP.h>
-#include <boost/noncopyable.hpp>
 #include <memory>
+
 
 namespace dcp {
 
+
 template <class R, class F>
-class AssetReader : public boost::noncopyable
+class AssetReader
 {
 public:
 	explicit AssetReader (Asset const * asset, boost::optional<Key> key, Standard standard)
-		: _crypto_context (new DecryptionContext (key, standard))
+		: _crypto_context (new DecryptionContext(key, standard))
 	{
 		_reader = new R ();
-		DCP_ASSERT (asset->file ());
-		Kumu::Result_t const r = _reader->OpenRead (asset->file()->string().c_str());
-		if (ASDCP_FAILURE (r)) {
+		DCP_ASSERT (asset->file());
+		auto const r = _reader->OpenRead (asset->file()->string().c_str());
+		if (ASDCP_FAILURE(r)) {
 			delete _reader;
-			boost::throw_exception (FileError ("could not open MXF file for reading", asset->file().get(), r));
+			boost::throw_exception (FileError("could not open MXF file for reading", asset->file().get(), r));
 		}
 	}
+
+	AssetReader (AssetReader const&) = delete;
+	AssetReader& operator== (AssetReader const&) = delete;
 
 	~AssetReader ()
 	{
@@ -66,7 +72,7 @@ public:
 
 	std::shared_ptr<const F> get_frame (int n) const
 	{
-		return std::shared_ptr<const F> (new F (_reader, n, _crypto_context));
+		return std::shared_ptr<const F> (new F(_reader, n, _crypto_context));
 	}
 
 	R* reader () const {
@@ -74,10 +80,12 @@ public:
 	}
 
 protected:
-	R* _reader;
+	R* _reader = nullptr;
 	std::shared_ptr<DecryptionContext> _crypto_context;
 };
 
+
 }
+
 
 #endif

src/asset_writer.cc
@@ -1,5 +1,5 @@
 /*
-    Copyright (C) 2012-2015 Carl Hetherington <cth@carlh.net>
+    Copyright (C) 2012-2021 Carl Hetherington <cth@carlh.net>
 
     This file is part of libdcp.
 
@@ -31,18 +31,22 @@
     files in the program, then also delete it here.
 */
 
+
 /** @file  src/asset_writer.h
  *  @brief AssetWriter class.
  */
 
+
 #include "asset_writer.h"
-#include "mxf.h"
-#include "dcp_assert.h"
 #include "crypto_context.h"
+#include "dcp_assert.h"
+#include "mxf.h"
 #include <asdcp/AS_DCP.h>
 #include <asdcp/KM_prng.h>
 
+
 using namespace dcp;
+
 
 /** Create an AssetWriter.
  *  @param mxf MXF that we are writing.
@@ -51,13 +55,11 @@ using namespace dcp;
 AssetWriter::AssetWriter (MXF* mxf, boost::filesystem::path file)
 	: _mxf (mxf)
 	, _file (file)
-	, _frames_written (0)
-	, _finalized (false)
-	, _started (false)
-	, _crypto_context (new EncryptionContext (mxf->key(), mxf->standard()))
+	, _crypto_context(new EncryptionContext (mxf->key(), mxf->standard()))
 {
 
 }
+
 
 /** @return true if anything was written by this writer */
 bool

src/asset_writer.h
@@ -1,5 +1,5 @@
 /*
-    Copyright (C) 2012-2015 Carl Hetherington <cth@carlh.net>
+    Copyright (C) 2012-2021 Carl Hetherington <cth@carlh.net>
 
     This file is part of libdcp.
 
@@ -31,21 +31,26 @@
     files in the program, then also delete it here.
 */
 
+
 /** @file  src/asset_writer.h
  *  @brief AssetWriter class.
  */
 
+
 #ifndef LIBDCP_ASSET_WRITER_H
 #define LIBDCP_ASSET_WRITER_H
+
 
 #include "types.h"
 #include "crypto_context.h"
 #include <boost/filesystem.hpp>
-#include <boost/noncopyable.hpp>
+
 
 namespace dcp {
 
+
 class MXF;
+
 
 /** @class AssetWriter
  *  @brief Parent class for classes which can write MXF-based assets.
@@ -53,9 +58,12 @@ class MXF;
  *  The AssetWriter lasts for the duration of the write and is then discarded.
  *  They can only be created by calling start_write() on an appropriate Asset object.
  */
-class AssetWriter : public boost::noncopyable
+class AssetWriter
 {
 public:
+	AssetWriter (AssetWriter const&) = delete;
+	AssetWriter& operator= (AssetWriter const&) = delete;
+
 	virtual ~AssetWriter () {}
 	virtual bool finalize ();
 
@@ -67,17 +75,17 @@ protected:
 	AssetWriter (MXF* mxf, boost::filesystem::path file);
 
 	/** MXF that we are writing */
-	MXF* _mxf;
+	MXF* _mxf = nullptr;
 	/** File that we are writing to */
 	boost::filesystem::path _file;
 	/** Number of `frames' written so far; the definition of a frame
 	 *  varies depending on the subclass.
 	 */
-	int64_t _frames_written;
+	int64_t _frames_written = 0;
 	/** true if finalize() has been called on this object */
-	bool _finalized;
+	bool _finalized = false;
 	/** true if something has been written to this asset */
-	bool _started;
+	bool _started = false;
 	std::shared_ptr<EncryptionContext> _crypto_context;
 };
 

src/certificate.cc
@@ -1,5 +1,5 @@
 /*
-    Copyright (C) 2012-2016 Carl Hetherington <cth@carlh.net>
+    Copyright (C) 2012-2021 Carl Hetherington <cth@carlh.net>
 
     This file is part of libdcp.
 
@@ -31,9 +31,11 @@
     files in the program, then also delete it here.
 */
 
+
 /** @file  src/certificate.cc
  *  @brief Certificate class.
  */
+
 
 #include "certificate.h"
 #include "compose.hpp"
@@ -51,52 +53,42 @@
 #include <iostream>
 #include <algorithm>
 
+
 using std::list;
 using std::string;
 using std::ostream;
 using std::min;
 using namespace dcp;
 
+
 static string const begin_certificate = "-----BEGIN CERTIFICATE-----";
 static string const end_certificate = "-----END CERTIFICATE-----";
 
-/** @param c X509 certificate, which this object will take ownership of */
+
 Certificate::Certificate (X509* c)
 	: _certificate (c)
-	, _public_key (0)
 {
 
 }
 
-/** Load an X509 certificate from a string.
- *  @param cert String to read from.
- */
+
 Certificate::Certificate (string cert)
-	: _certificate (0)
-	, _public_key (0)
 {
-	string const s = read_string (cert);
-	if (!s.empty ()) {
+	auto const s = read_string (cert);
+	if (!s.empty()) {
 		throw MiscError ("unexpected data after certificate");
 	}
 }
 
-/** Copy constructor.
- *  @param other Certificate to copy.
- */
+
 Certificate::Certificate (Certificate const & other)
-	: _certificate (0)
-	, _public_key (0)
 {
 	if (other._certificate) {
 		read_string (other.certificate (true));
 	}
 }
 
-/** Read a certificate from a string.
- *  @param cert String to read.
- *  @return remaining part of the input string after the certificate which was read.
- */
+
 string
 Certificate::read_string (string cert)
 {
@@ -121,7 +113,7 @@ Certificate::read_string (string cert)
 		lines.push_back (line);
 	}
 
-	list<string>::iterator i = lines.begin ();
+	auto i = lines.begin ();
 
 	/* BEGIN */
 	while (i != lines.end() && *i != begin_certificate) {
@@ -165,7 +157,7 @@ Certificate::read_string (string cert)
 
 	fixed += end_certificate;
 
-	BIO* bio = BIO_new_mem_buf (const_cast<char *> (fixed.c_str ()), -1);
+	auto bio = BIO_new_mem_buf (const_cast<char *> (fixed.c_str ()), -1);
 	if (!bio) {
 		throw MiscError ("could not create memory BIO");
 	}
@@ -189,16 +181,14 @@ Certificate::read_string (string cert)
 	return extra;
 }
 
-/** Destructor */
+
 Certificate::~Certificate ()
 {
 	X509_free (_certificate);
 	RSA_free (_public_key);
 }
 
-/** operator= for Certificate.
- *  @param other Certificate to read from.
- */
+
 Certificate &
 Certificate::operator= (Certificate const & other)
 {
@@ -211,21 +201,18 @@ Certificate::operator= (Certificate const & other)
 	RSA_free (_public_key);
 	_public_key = 0;
 
-	read_string (other.certificate (true));
+	read_string (other.certificate(true));
 
 	return *this;
 }
 
-/** Return the certificate as a string.
- *  @param with_begin_end true to include the -----BEGIN CERTIFICATE--- / -----END CERTIFICATE----- markers.
- *  @return Certificate string.
- */
+
 string
 Certificate::certificate (bool with_begin_end) const
 {
 	DCP_ASSERT (_certificate);
 
-	BIO* bio = BIO_new (BIO_s_mem ());
+	auto bio = BIO_new (BIO_s_mem());
 	if (!bio) {
 		throw MiscError ("could not create memory BIO");
 	}
@@ -249,16 +236,14 @@ Certificate::certificate (bool with_begin_end) const
 	return s;
 }
 
-/** @return Certificate's issuer, in the form
- *  dnqualifier=&lt;dnQualififer&gt;,CN=&lt;commonName&gt;,OU=&lt;organizationalUnitName&gt,O=&lt;organizationName&gt;
- *  and with + signs escaped to \+
- */
+
 string
 Certificate::issuer () const
 {
 	DCP_ASSERT (_certificate);
-	return name_for_xml (X509_get_issuer_name (_certificate));
+	return name_for_xml (X509_get_issuer_name(_certificate));
 }
+
 
 string
 Certificate::asn_to_utf8 (ASN1_STRING* s)
@@ -270,6 +255,7 @@ Certificate::asn_to_utf8 (ASN1_STRING* s)
 	return u;
 }
 
+
 string
 Certificate::get_name_part (X509_NAME* n, int nid)
 {
@@ -278,15 +264,16 @@ Certificate::get_name_part (X509_NAME* n, int nid)
 	if (p == -1) {
 		return "";
 	}
-	return asn_to_utf8 (X509_NAME_ENTRY_get_data (X509_NAME_get_entry (n, p)));
+	return asn_to_utf8 (X509_NAME_ENTRY_get_data(X509_NAME_get_entry(n, p)));
 }
+
 
 string
 Certificate::name_for_xml (X509_NAME* name)
 {
 	assert (name);
 
-	BIO* bio = BIO_new (BIO_s_mem ());
+	auto bio = BIO_new (BIO_s_mem ());
 	if (!bio) {
 		throw MiscError ("could not create memory BIO");
 	}
@@ -310,32 +297,36 @@ Certificate::subject () const
 {
 	DCP_ASSERT (_certificate);
 
-	return name_for_xml (X509_get_subject_name (_certificate));
+	return name_for_xml (X509_get_subject_name(_certificate));
 }
+
 
 string
 Certificate::subject_common_name () const
 {
 	DCP_ASSERT (_certificate);
 
-	return get_name_part (X509_get_subject_name (_certificate), NID_commonName);
+	return get_name_part (X509_get_subject_name(_certificate), NID_commonName);
 }
+
 
 string
 Certificate::subject_organization_name () const
 {
 	DCP_ASSERT (_certificate);
 
-	return get_name_part (X509_get_subject_name (_certificate), NID_organizationName);
+	return get_name_part (X509_get_subject_name(_certificate), NID_organizationName);
 }
+
 
 string
 Certificate::subject_organizational_unit_name () const
 {
 	DCP_ASSERT (_certificate);
 
-	return get_name_part (X509_get_subject_name (_certificate), NID_organizationalUnitName);
+	return get_name_part (X509_get_subject_name(_certificate), NID_organizationalUnitName);
 }
+
 
 static
 struct tm
@@ -359,6 +350,7 @@ convert_time (ASN1_TIME const * time)
 	return t;
 }
 
+
 struct tm
 Certificate::not_before () const
 {
@@ -369,6 +361,7 @@ Certificate::not_before () const
 	return convert_time(X509_get_notBefore(_certificate));
 #endif
 }
+
 
 struct tm
 Certificate::not_after () const
@@ -381,15 +374,16 @@ Certificate::not_after () const
 #endif
 }
 
+
 string
 Certificate::serial () const
 {
 	DCP_ASSERT (_certificate);
 
-	ASN1_INTEGER* s = X509_get_serialNumber (_certificate);
+	auto s = X509_get_serialNumber (_certificate);
 	DCP_ASSERT (s);
 
-	BIGNUM* b = ASN1_INTEGER_to_BN (s, 0);
+	auto b = ASN1_INTEGER_to_BN (s, 0);
 	char* c = BN_bn2dec (b);
 	BN_free (b);
 
@@ -399,7 +393,7 @@ Certificate::serial () const
 	return st;
 }
 
-/** @return thumbprint of the to-be-signed portion of this certificate */
+
 string
 Certificate::thumbprint () const
 {
@@ -428,7 +422,7 @@ Certificate::thumbprint () const
 	return Kumu::base64encode (digest, 20, digest_base64, 64);
 }
 
-/** @return RSA public key from this Certificate.  Caller must not free the returned value. */
+
 RSA *
 Certificate::public_key () const
 {
@@ -438,7 +432,7 @@ Certificate::public_key () const
 		return _public_key;
 	}
 
-	EVP_PKEY* key = X509_get_pubkey (_certificate);
+	auto key = X509_get_pubkey (_certificate);
 	if (!key) {
 		throw MiscError ("could not get public key from certificate");
 	}
@@ -451,6 +445,7 @@ Certificate::public_key () const
 	return _public_key;
 }
 
+
 static bool string_is_utf8 (X509_NAME* n, int nid)
 {
 	int p = -1;
@@ -458,14 +453,16 @@ static bool string_is_utf8 (X509_NAME* n, int nid)
 	return p != -1 && X509_NAME_ENTRY_get_data(X509_NAME_get_entry(n, p))->type == V_ASN1_UTF8STRING;
 }
 
+
 bool
 Certificate::has_utf8_strings () const
 {
-	X509_NAME* n = X509_get_subject_name (_certificate);
+	auto n = X509_get_subject_name (_certificate);
 	return string_is_utf8(n, NID_commonName) ||
 		string_is_utf8(n, NID_organizationName) ||
 		string_is_utf8(n, NID_organizationalUnitName);
 }
+
 
 bool
 dcp::operator== (Certificate const & a, Certificate const & b)
@@ -473,11 +470,13 @@ dcp::operator== (Certificate const & a, Certificate const & b)
 	return a.certificate() == b.certificate();
 }
 
+
 bool
 dcp::operator< (Certificate const & a, Certificate const & b)
 {
 	return a.certificate() < b.certificate();
 }
+
 
 ostream&
 dcp::operator<< (ostream& s, Certificate const & c)

src/certificate.h
@@ -1,5 +1,5 @@
 /*
-    Copyright (C) 2012-2016 Carl Hetherington <cth@carlh.net>
+    Copyright (C) 2012-2021 Carl Hetherington <cth@carlh.net>
 
     This file is part of libdcp.
 
@@ -31,12 +31,15 @@
     files in the program, then also delete it here.
 */
 
+
 /** @file  src/certificate.h
  *  @brief Certificate class.
  */
 
+
 #ifndef LIBDCP_CERTIFICATE_H
 #define LIBDCP_CERTIFICATE_H
+
 
 #undef X509_NAME
 #include <openssl/x509.h>
@@ -44,16 +47,19 @@
 #include <string>
 #include <list>
 
+
 namespace xmlpp {
 	class Element;
 }
 
+
 namespace dcp {
 
+
 /** @class Certificate
- *  @brief A wrapper for an X509 certificate.
+ *  @brief A wrapper for an X509 certificate
  *
- *  This class can take a Certificate from a string or an OpenSSL X509 object.
+ *  This class can take a Certificate from a string or an OpenSSL X509 object
  */
 class Certificate
 {
@@ -63,18 +69,37 @@ public:
 		, _public_key (0)
 	{}
 
+	/** Load an X509 certificate from a string
+	 *  @param cert String to read from
+	 */
 	explicit Certificate (std::string);
+
+	/** @param c X509 certificate, which this object will take ownership of */
 	explicit Certificate (X509 *);
+
 	Certificate (Certificate const &);
 	~Certificate ();
 
 	Certificate& operator= (Certificate const &);
 
+	/** Read a certificate from a string.
+	 *  @param cert String to read.
+	 *  @return remaining part of the input string after the certificate which was read.
+	 */
 	std::string read_string (std::string);
 
+	/** Return the certificate as a string
+	 *  @param with_begin_end true to include the -----BEGIN CERTIFICATE--- / -----END CERTIFICATE----- markers
+	 *  @return Certificate string
+	 */
 	std::string certificate (bool with_begin_end = false) const;
+
 	std::string serial () const;
 
+	/** @return Certificate's issuer, in the form
+	 *  dnqualifier=&lt;dnQualififer&gt;,CN=&lt;commonName&gt;,OU=&lt;organizationalUnitName&gt,O=&lt;organizationName&gt;
+	 *  and with + signs escaped to \+
+	 */
 	std::string issuer () const;
 
 	std::string subject () const;
@@ -88,8 +113,10 @@ public:
 		return _certificate;
 	}
 
+	/** @return RSA public key from this Certificate.  Caller must not free the returned value. */
 	RSA* public_key () const;
 
+	/** @return thumbprint of the to-be-signed portion of this certificate */
 	std::string thumbprint () const;
 
 	bool has_utf8_strings () const;
@@ -100,13 +127,15 @@ private:
 	static std::string asn_to_utf8 (ASN1_STRING *);
 	static std::string get_name_part (X509_NAME *, int);
 
-	X509* _certificate;
-	mutable RSA* _public_key;
+	X509* _certificate = nullptr;
+	mutable RSA* _public_key = nullptr;
 };
+
 
 bool operator== (Certificate const & a, Certificate const & b);
 bool operator< (Certificate const & a, Certificate const & b);
 std::ostream& operator<< (std::ostream&s, Certificate const & c);
+
 
 }
 

commit a31aee3f2909992ac717ecc72441240e7d1a56f7

Fix strange Windows build error introduced in 6c37cc1979b2a01205a888c4c98f3334685ee8dd

src/certificate_chain.cc
@@ -78,7 +78,7 @@ command (string cmd)
 	   is handled correctly.
 	*/
 	int const wn = MultiByteToWideChar (CP_UTF8, 0, cmd.c_str(), -1, 0, 0);
-	char buffer = new wchar_t[wn];
+	auto buffer = new wchar_t[wn];
 	if (MultiByteToWideChar (CP_UTF8, 0, cmd.c_str(), -1, buffer, wn) == 0) {
 		delete[] buffer;
 		return;