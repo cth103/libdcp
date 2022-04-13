22781828732837911d0dc9b60acdac7e98e673c8
Carl Hetherington <cth@carlh.net>
2022-04-14 00:36:24 +0200

    Move fix_long_path tests over from DoM.

---
+++ test/file_test.cc
@@ -0,0 +1,87 @@
+/*
+    Copyright (C) 2022 Carl Hetherington <cth@carlh.net>
+
+    This file is part of libdcp.
+
+    libdcp is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    libdcp is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with libdcp.  If not, see <http://www.gnu.org/licenses/>.
+
+    In addition, as a special exception, the copyright holders give
+    permission to link the code of portions of this program with the
+    OpenSSL library under certain conditions as described in each
+    individual source file, and distribute linked combinations
+    including the two.
+
+    You must obey the GNU General Public License in all respects
+    for all of the code used other than OpenSSL.  If you modify
+    file(s) with this exception, you may extend this exception to your
+    version of the file(s), but you are not obligated to do so.  If you
+    do not wish to do so, delete this exception statement from your
+    version.  If you delete this exception statement from all source
+    files in the program, then also delete it here.
+*/
+
+
+#include "file.h"
+#include <boost/test/unit_test.hpp>
+
+
+BOOST_AUTO_TEST_CASE (fix_long_path_test)
+{
+#ifdef LIBDCP_WINDOWS
+	BOOST_CHECK_EQUAL (dcp::fix_long_path("c:\\foo"), "\\\\?\\c:\\foo");
+	BOOST_CHECK_EQUAL (dcp::fix_long_path("c:\\foo\\bar"), "\\\\?\\c:\\foo\\bar");
+	boost::filesystem::path fixed_bar = "\\\\?\\";
+	fixed_bar += boost::filesystem::current_path();
+	fixed_bar /= "bar";
+	BOOST_CHECK_EQUAL (dcp::fix_long_path("bar"), fixed_bar);
+
+	BOOST_CHECK_EQUAL (dcp::fix_long_path("\\\\?\\c:\\foo"), "\\\\?\\c:\\foo");
+#else
+	BOOST_CHECK_EQUAL (dcp::fix_long_path("foo/bar/baz"), "foo/bar/baz");
+#endif
+}
+
+
+#ifdef LIBDCP_WINDOWS
+BOOST_AUTO_TEST_CASE (windows_long_filename_test)
+{
+	using namespace boost::filesystem;
+
+	path too_long = current_path() / "build\\test\\a\\really\\very\\long\\filesystem\\path\\indeed\\that\\will\\be\\so\\long\\that\\windows\\cannot\\normally\\cope\\with\\it\\unless\\we\\add\\this\\crazy\\prefix\\and\\then\\magically\\it\\can\\do\\it\\fine\\I\\dont\\really\\know\\why\\its\\like\\that\\but\\hey\\it\\is\\so\\here\\we\\are\\what\\can\\we\\do\\other\\than\\bodge\\it";
+
+	BOOST_CHECK (too_long.string().length() > 260);
+	boost::system::error_code ec;
+	create_directories (too_long, ec);
+	BOOST_CHECK (ec);
+
+	path fixed_path = dcp::fix_long_path(too_long);
+	create_directories (fixed_path, ec);
+	BOOST_CHECK (!ec);
+
+	{
+		dcp::File file(too_long / "hello", "w");
+		BOOST_REQUIRE (file);
+		fprintf (file.get(), "Hello_world");
+	}
+
+	{
+		dcp::File file(too_long / "hello", "r");
+		BOOST_REQUIRE (file);
+		char buffer[64];
+		fscanf (file.get(), "%63s", buffer);
+		BOOST_CHECK_EQUAL (strcmp(buffer, "Hello_world"), 0);
+	}
+}
+#endif
+
