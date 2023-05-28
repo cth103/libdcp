970c75b75f9acc5dad4f3df10938687552b57874
Carl Hetherington <cth@carlh.net>
2023-05-28 02:12:39 +0200

    Don't unconditionally clear _fonts when inspecting assets (DoM #2536).
    resolve_fonts() is sometimes called for the same asset with different
    lists of things to check.  If it is called first with a font, and
    then without one, we want to keep the font from the first call.

--- src/interop_subtitle_asset.cc
+++ src/interop_subtitle_asset.cc
@@ -230,8 +230,6 @@ InteropSubtitleAsset::write (boost::filesystem::path p) const
 void
 InteropSubtitleAsset::resolve_fonts (vector<shared_ptr<Asset>> assets)
 {
-	_fonts.clear();
-
 	for (auto asset: assets) {
 		auto font = dynamic_pointer_cast<FontAsset>(asset);
 		if (!font) {
@@ -243,7 +241,12 @@ InteropSubtitleAsset::resolve_fonts (vector<shared_ptr<Asset>> assets)
 		for (auto load_font_node: _load_font_nodes) {
 			auto const path_in_load_font_node = _file->parent_path() / load_font_node->uri;
 			if (font->file() && path_in_load_font_node == *font->file()) {
-				_fonts.push_back(Font(load_font_node->id, asset->id(), font->file().get()));
+				auto existing = std::find_if(_fonts.begin(), _fonts.end(), [load_font_node](Font const& font) { return font.load_id == load_font_node->id; });
+				if (existing != _fonts.end()) {
+					*existing = Font(load_font_node->id, asset->id(), font->file().get());
+				} else {
+					_fonts.push_back(Font(load_font_node->id, asset->id(), font->file().get()));
+				}
 			}
 		}
 	}
