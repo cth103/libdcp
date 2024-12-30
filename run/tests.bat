233635d702090a2e7d70cc04780fb95d189c732b
Carl Hetherington <cth@carlh.net>
2024-12-30 12:36:53 +0100

    Switch to 2.18.x environment for Windows too.

--- run/tests.bat
+++ run/tests.bat
@@ -1,4 +1,4 @@
-set PATH=%PATH%;c:\users\ci\bin;c:\users\ci\workspace\libdcp\bin;c:\users\ci\workspace\libdcp\lib
+set PATH=%PATH%;c:\users\ci\bin_v2.17.x;c:\users\ci\workspace\libdcp\bin;c:\users\ci\workspace\libdcp\lib
 xcopy ..\libdcp\tags build\tags\
 xcopy ..\libdcp\ratings build\
 build\test\tests.exe --log_level=test_suite -- c:\users\ci\libdcp-test-private
