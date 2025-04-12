05adc13a5eef7c0fe165d062c0e40ad558a083d0
Carl Hetherington <cth@carlh.net>
2025-04-12 15:15:06 +0200

    Coalesce an if.

--- src/verify.cc
+++ src/verify.cc
@@ -1190,8 +1190,7 @@ dcp::verify_text_lines_and_characters(
 
 	/* Make a list of "subtitle starts" and "subtitle ends" events */
 	for (auto j: asset->texts()) {
-		auto text = dynamic_pointer_cast<const TextString>(j);
-		if (text) {
+		if (auto text = dynamic_pointer_cast<const TextString>(j)) {
 			auto in = make_shared<Event>(text->in(), position(text), text->text().length());
 			events.push_back(in);
 			events.push_back(make_shared<Event>(text->out(), in));
